4b18dfad0f36a0bff5faf29194853bc393911020
Marco Paulo Ollivier <marcopollivier@users.noreply.github.com>
2011-12-16 02:05:21 +0000

    listagem de processo no linux

--- InformaçõesDoSistema/src/felipe/model/scanner/UnixProcessScanner.java
+++ InformaçõesDoSistema/src/felipe/model/scanner/UnixProcessScanner.java
@@ -1,10 +1,26 @@
 package felipe.model.scanner;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class UnixProcessScanner implements ProcessScanner{
 
 	@Override
 	public void readAllProcess() {
-		System.out.println("opa.. falta implementar..");
+		
+		try {
+			Process p = Runtime.getRuntime().exec("ps ax"); //aqui fica o comando que vai pegar os processos  
+			
+			BufferedReader resultado = new BufferedReader(new InputStreamReader(p.getInputStream()));  
+			
+			//mostra os resultados obtidos pelo comando 'ps ax'  
+			String s;  
+			while ((s = resultado.readLine()) != null)  
+				System.out.println(s);  
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
